daf6c35623be09684af4d489bdd4db77beff38dd
Robert Detjens <detjensrobert@gmail.com>
2020-11-07 11:40:47 -0800

    add most of cs340 pg1
    Signed-off-by: Robert Detjens <detjensrobert@gmail.com>

---
+++ cs340/program1/cs340-program1.sql
@@ -0,0 +1,101 @@
+-- CS 340 Assignment 1
+-- Robert Detjens
+
+-- 1. Create the table DEPT_STATS as shown below:
+
+create table `DEPT_STATS` (
+  `Dnumber` int(2) not null,
+  `Emp_count` int(11) not null,
+  `Avg_salary` decimal(10,2) not null,
+  primary key (`Dnumber`),
+  foreign key (`Dnumber`) references `DEPARTMENT` (`Dnumber`)
+) ENGINE = INNODB;
+
+-- 2. Write a procedure called InitDeptStats to initialize the table.
+
+delimiter !
+create procedure InitDeptStats()
+begin
+  insert into DEPT_STATS (Dnumber, Emp_count, Avg_salary)
+    select E.Dno, count(*), avg(E.Salary)
+    from EMPLOYEE E
+    group by E.Dno;
+end!
+delimiter ;
+
+-- 3. Write triggers for the EMPLOYEE table that modify the DEPT_STATS table
+--    when rows are inserted, deleted or updated in the EMPLOYEE table.
+
+delimiter !
+create procedure UpdateDeptStats()
+begin
+  replace into DEPT_STATS (Dnumber, Emp_count, Avg_salary)
+    select E.Dno, count(*), avg(E.Salary)
+    from EMPLOYEE E
+    group by E.Dno;
+end!
+delimiter ;
+
+create trigger `DELETEDeptStats`
+after delete on `EMPLOYEE`
+for each row
+call UpdateDeptStats();
+
+create trigger `INSERTDeptStats`
+after insert on `EMPLOYEE`
+for each row
+call UpdateDeptStats();
+
+create trigger `UPDATEDeptStats`
+after update on `EMPLOYEE`
+for each row
+call UpdateDeptStats();
+
+-- 4. Write a trigger called MaxTotalHours for the WORKS_ON table that will
+--    generate user defined error message if an attempt is made to assign the
+--    employee more than 40 hours.
+
+delimiter !
+create trigger `MaxTotalHours`
+before insert on `WORKS_ON`
+for each row
+begin
+  declare hoursWorked decimal(3,1);
+
+  select sum(Hours)
+  into hoursWorked
+  from WORKS_ON WO
+  where WO.Essn = NEW.Essn
+  group by Essn;
+
+  if (hoursWorked > 40) then
+    signal SQLSTATE '45000' set MESSAGE_TEXT = 'ERR: Employee hours exceeds 40h';
+  end if;
+end!
+delimiter ;
+
+-- 5. Write a function called PayLevel that given an Ssn as input, returns:
+--    "Above Average" if the employee makes more than the department average
+--    "Average" if the employee makes the same as the department average
+--    "Below Average" if the employee makes less than the department average
+
+delimiter !
+create function PayLevel(
+  Ssn char(9)
+)
+returns varchar(25)
+begin
+  declare result varchar(25);
+
+  select case
+    when E.Salary > DS.Avg_salary then "Above Average"
+    when E.Salary < DS.Avg_salary then "Below Average"
+    else "Average"
+  end into result
+  from EMPLOYEE E, DEPT_STATS DS
+  where E.Ssn = Ssn
+    and E.Dno = DS.Dnumber;
+
+  return result;
+end!
+delimiter ;
